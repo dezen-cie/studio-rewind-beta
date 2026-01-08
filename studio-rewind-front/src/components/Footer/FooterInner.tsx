2e8647f4eaaa3f9594497737e182b0a66a0d5843
dezem <dezem.cie@gmail.com>
2026-01-08 14:01:37 +0100

    add instagram link in footer

--- studio-rewind-front/src/components/Footer/FooterInner.tsx
+++ studio-rewind-front/src/components/Footer/FooterInner.tsx
@@ -51,7 +51,7 @@ function FooterInner(){
             </nav>
 
             <div className="footer-social">
-              <a href="https://www.instagram.com/" className="icon-insta" target="_blank" rel="noreferrer">
+              <a href="https://www.instagram.com/studio_rewind_official/" className="icon-insta" target="_blank" rel="noreferrer">
                 <Instagram />
               </a>
               <a href="https://www.facebook.com/" className="icon-fb" target="_blank" rel="noreferrer">
