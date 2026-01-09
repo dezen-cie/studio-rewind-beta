9ca54138064c6df88991604c854a8c6d96e29ef1
dezem <dezem.cie@gmail.com>
2026-01-09 15:50:22 +0100

    add tiktok link & remove facebook link

--- studio-rewind-front/src/components/Footer/FooterInner.tsx
+++ studio-rewind-front/src/components/Footer/FooterInner.tsx
@@ -1,6 +1,20 @@
 import { Link, useLocation } from 'react-router-dom'
-import { Instagram, Facebook, ArrowUp } from 'lucide-react'
+import { Instagram, ArrowUp } from 'lucide-react'
 import './FooterInner.css'
+
+function TikTokIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
+    </svg>
+  )
+}
 
 
 function FooterInner(){
@@ -58,8 +72,8 @@ function FooterInner(){
               <a href="https://www.instagram.com/studio_rewind_official/" className="icon-insta" target="_blank" rel="noreferrer">
                 <Instagram />
               </a>
-              <a href="https://www.facebook.com/" className="icon-fb" target="_blank" rel="noreferrer">
-                <Facebook />
+              <a href="https://www.tiktok.com/@studio_rewind" className="icon-tiktok" target="_blank" rel="noreferrer">
+                <TikTokIcon />
               </a>
               <button
                 type="button"
